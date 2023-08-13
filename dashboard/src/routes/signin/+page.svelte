4e173d1d3c8aec8c5abe081e8de14d85e3de0b2c
prokawsar <prokawsar@gmail.com>
2023-08-13 05:52:08 -0400

    sign in sign up page

---
+++ dashboard/src/routes/signin/+page.svelte
@@ -0,0 +1,76 @@
+<div class="container mt-20">
+	<div class="flex flex-wrap">
+		<div
+			class="w-full max-w-full px-3 mx-auto mt-0 md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12"
+		>
+			<div
+				class="outline outline-1 shadow-md outline-teal-500 relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border"
+			>
+				<div class="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
+					<h3
+						class="relative text-2xl z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text"
+					>
+						Welcome back
+					</h3>
+					<p class="text-gray-500">Enter your email and password to sign in</p>
+				</div>
+
+				<div class="flex-auto p-6">
+					<form>
+						<div class="mb-4">
+							<input
+								type="email"
+								class="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border-2 border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-teal-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+								placeholder="Email"
+								aria-label="Email"
+								aria-describedby="email-addon"
+							/>
+						</div>
+						<div class="mb-4">
+							<input
+								type="password"
+								class="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border-2 border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-teal-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+								placeholder="Password"
+								aria-label="Password"
+								aria-describedby="password-addon"
+							/>
+						</div>
+						<div class="min-h-6 mb-1 block pl-12">
+							<input
+								id="rememberMe"
+								class="mt-1 rounded-full duration-250 ease-in-out after:rounded-full after:shadow-2xl after:duration-250 checked:after:translate-x-5 h-5 relative float-left -ml-12 w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right"
+								type="checkbox"
+							/>
+							<label
+								class="mb-2 ml-1 font-normal cursor-pointer select-none text-sm text-slate-700"
+								for="rememberMe"
+							>
+								Remember me
+							</label>
+						</div>
+
+						<div class="text-center">
+							<button
+								type="button"
+								class="inline-block w-full px-6 py-3 mt-6 mb-2 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-800 hover:border-slate-700 hover:bg-slate-700 hover:text-white"
+							>
+								Sign in
+							</button>
+						</div>
+					</form>
+					<div class="p-6 mt-10 px-1 pt-0 text-center lg:px-2">
+						<p class="mx-auto leading-normal text-sm">
+							Don't have an account?
+							<a
+								href="/signup"
+								class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text"
+							>
+								Sign up
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
