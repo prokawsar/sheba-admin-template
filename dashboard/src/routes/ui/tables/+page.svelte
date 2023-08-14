edcf2f289584a5ee1ed8dd4ba55a8db30aa71c40
Kawsar Ahmed <prokawsar@gmail.com>
2023-08-14 19:42:39 +0600

    push

--- dashboard/src/routes/ui/tables/+page.svelte
+++ dashboard/src/routes/ui/tables/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col gap-3 px-2">
-	<div class="w-full max-w-full mt-0 md:w-1/2 md:flex-none lg:w-2/3 lg:flex-none">
+	<div class="w-full max-w-full mt-0 md:flex-none lg:flex-none">
 		<Table />
 	</div>
 </div>
